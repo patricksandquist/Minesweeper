0971687655458538a8c0fa167eacf52cb4036819
Patrick Sandquist <patricksandquist@gmail.com>
2015-10-05 13:33:47 -0700

    complete weather

--- weather.js
+++ weather.js
@@ -1,9 +1,11 @@
 var Clock = React.createClass( {
   render: function() {
-    return (<div>
-                {this.state.currentTime}
-                <Weather/>
-            </div>);
+    return (
+      <div>
+        {this.state.currentTime}
+        <Weather/>
+      </div>
+    );
   },
 
   getInitialState: function() {
@@ -29,7 +31,33 @@ var Clock = React.createClass( {
 
 var Weather = React.createClass({
   render: function() {
-    return <div>Longitude: {this.state.loc[0]}<br/>Latitude: {this.state.loc[1]}</div>
+    return <div>Longitude: {this.state.loc[0]}<br/>Latitude: {this.state.loc[1]}<div>{this.state.weather}</div></div>
+  },
+
+  getWeather: function() {
+    var request = new XMLHttpRequest();
+    request.open('GET', 'http://api.openweathermap.org/data/2.5/weather?lat=' +
+                        this.state.loc[0] + '&lon=' + this.state.loc[1], true);
+
+    request.onload = function() {
+      if (request.status >= 200 && request.status < 400) {
+        // Success!
+        var resp = request.responseText;
+        this.setState({weather: resp});
+      } else {
+        // We reached our target server, but it returned an error
+      }
+    }.bind(this);
+
+    request.onerror = function() {
+      // There was a connection error of some sort
+    };
+
+    request.send();
+  },
+
+  componentDidMount: function() {
+    this.getWeather();
   },
 
   getInitialState: function() {
@@ -37,9 +65,10 @@ var Weather = React.createClass({
     // var loc;
     var that = this;
     navigator.geolocation.getCurrentPosition(function(pos) {
-      var locat = [pos.coords.latitude, pos.coords.longitude]
-      // console.log(locat);
-      that.setState({loc: locat});
+      var location = [pos.coords.latitude, pos.coords.longitude]
+      // console.log(location);
+      that.setState({loc: location});
+      that.getWeather();
     });
     return {loc: "  "}
   }
